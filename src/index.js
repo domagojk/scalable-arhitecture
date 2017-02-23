f0fc5d22a352a32c2b0c015965e20594d01b35da
Domagoj Kriskovic <domagoj.kriskovic@gmail.com>
2017-02-23 02:16:47 +0100

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Rx from 'rxjs/Rx'
 import ReactDOM from 'react-dom'
 import createRecycle from 'recyclejs/react'
+
 import Wrapper from './components/Wrapper'
 import actions from './drivers/actions'
 import store from './drivers/store'
