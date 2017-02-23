9aa8d1cfdc8c978622c1cd1e21f14ad6b0744120
Domagoj Kriskovic <domagoj.kriskovic@gmail.com>
2017-02-23 17:44:50 +0100

    url changed

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   documentEndpoint: path => `https://api.github.com/repos/${path}/contents/README.md?ref=master`,
-  shareDB: 'ws://localhost:8080'
+  shareDB: 'ws://Syncopath-vedranjukic202326.codeanyapp.com:8080'
 }
