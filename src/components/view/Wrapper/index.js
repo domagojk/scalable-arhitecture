73fc5cc59eaf4be4b32eecc32b0c6be8e598146d
Domagoj Kriskovic <domagoj.kriskovic@gmail.com>
2017-03-01 13:27:31 +0100

    wrapper comment

--- src/components/view/Wrapper/index.js
+++ src/components/view/Wrapper/index.js
@@ -1,10 +1,10 @@
 /**
- * This is not truly independent component
- * because it's importing Markdown and RepoList relatively.
- * Because of that, it's a part of the Domain logic
+ * Because Markdown and RepoList are imported relatively,
+ * this is not truly independent component,
+ * but a part of the Domain logic.
  *
- * Somethimes this is ok, but to make it scalable
- * it's prefered for Markdown and RepoList to be imported from npm
+ * Somethimes this is ok, but to make it independent and scalable
+ * Markdown and RepoList should be imported from npm
  */
 import React from 'react'
 import './style.css'
